c888ae81706f466caee2df08c00f9b0dae84647e
Ranjith <ranju08134@gmail.com>
2019-10-22 17:08:00 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express=require('express');
+
+const app=express();
+
+// Index Route
+
+app.get(('/'),(req,res)=>{
+    res.send('Welcome');
+});
+
+// About Route
+
+app.get(('/about'),(req,res)=>{
+    res.send('About');
+});
+
+const port=5000;
+
+app.listen(port,()=>{
+    console.log(`Server started on port:${port}`);
+})
