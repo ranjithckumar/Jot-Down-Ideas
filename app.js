b35509ca7992f1a6b8c35f2a88de7e43ae7bb1c2
Ranjith <ranju08134@gmail.com>
2019-10-23 12:17:52 +0530

    Added middlewares

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express=require('express');
+const exphbs=require('express-handlebars');
 
 const app=express();
-
+//  Handle-bars middleware
+app.engine('handlebars',exphbs({
+    defaultLayout:'main'
+}));
+app.set('view engine','handlebars');
 // Index Route
-
 app.get(('/'),(req,res)=>{
-    res.send('Welcome');
+    const add='Welcome!!!!';
+    res.render('index',{
+        title:add
+        });
 });
-
+  
 // About Route
-
 app.get(('/about'),(req,res)=>{
-    res.send('About');
+    res.send('About page');
 });
 
-const port=5000;
+const port=7000;
 
 app.listen(port,()=>{
     console.log(`Server started on port:${port}`);
