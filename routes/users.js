741a8e509e293c6fb4756fbb9fdcfcaacc19eef9
Ranjith <ranju08134@gmail.com>
2019-10-24 22:26:58 +0530

    Updated

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.get('/login',(req,res)=>{
 });
 // User register route
 router.get('/register',(req,res)=>{
-    res.send('register');
+    res.render('users/register');
 });
 
 module.exports=router; 
