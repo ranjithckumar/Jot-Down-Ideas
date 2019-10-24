3414eb3af7aa58307018d2103fb48be9f5c2ddf3
Ranjith <ranju08134@gmail.com>
2019-10-24 18:40:51 +0530

    Updated

--- app.js
+++ app.js
@@ -111,6 +111,7 @@ app.post('/ideas',(req,res)=>{
       new Idea(newUser)
       .save()
       .then(idea=>{
+        req.flash('success_msg','Video idea added');
           res.redirect('/ideas');
       })
   }
@@ -129,6 +130,7 @@ app.put('/ideas/:id',(req,res)=>{
        
        idea.save()
        .then(idea=>{
+        req.flash('success_msg','Video idea Updated');
            res.redirect('/ideas');
        })
   });
@@ -138,9 +140,11 @@ app.put('/ideas/:id',(req,res)=>{
 app.delete('/ideas/:id',(req,res)=>{
     Idea.remove({_id:req.params.id})
     .then(()=>{
+        req.flash('success_msg','Video idea removed');
         res.redirect('/ideas');
     });
 });
+
 const port=3000;
 
 app.listen(port,()=>{
