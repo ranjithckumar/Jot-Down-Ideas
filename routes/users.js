296d723aa92b586e17808be3a8a7790a323cd560
Ranjith <ranju08134@gmail.com>
2019-10-24 23:38:39 +0530

    Updated

--- routes/users.js
+++ routes/users.js
@@ -13,7 +13,26 @@ router.get('/register',(req,res)=>{
 
 // Register form post
 router.post('/register',(req,res)=>{
-    res.send('register');
+    let errors=[];
+    if(req.body.password !=req.body.password2){
+        errors.push({text:'Password do not match'})
+    }
+    if(req.body.password.length < 6){
+        errors.push({text:'Password must be at least 6 charcters'});
+    }
+    if(errors.length>0){
+        res.render('users/register',{
+            errors:errors,
+            name:req.body.name,
+            email:req.body.email,
+            password:req.body.password,
+            password2:req.body.password2
+            
+        });
+    }
+    else{
+        res.send('passed');
+    }
 });
 
 module.exports=router; 
