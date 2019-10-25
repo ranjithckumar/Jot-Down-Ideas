c34ce156a1b5b30584a3caf2ec80633dd48f343a
Ranjith <ranju08134@gmail.com>
2019-10-25 13:36:17 +0530

    Updated

--- config/passport.js
+++ config/passport.js
@@ -7,6 +7,32 @@ const bcrypt=require('bcryptjs');
 const User=mongoose.model('users');
 module.exports=function(passport){
    passport.use(new LocalStrategy({usernameField:'email'},(email,password,done)=>{
-       
+       User.findOne({
+           email:email
+       }).then(user=>{
+        //    Check for user 
+           if(!user){
+              return done(null,false,{message:'No user found'});
+           }
+        //    Check for password matching
+        bcrypt.compare(password,user.paasword,(err,isMatch)=>{
+             if(err) throw err;
+             if(isMatch){
+               return done(null,user);
+             }
+             else{
+                return done(null,false,{message:'Password Incorrect'});
+             }
+        })
+       })
    }));
+
+   passport.serializeUser(function(user,done){
+       done(user,user.id);
+   });
+   passport.deserializeUser(function(id,done){
+       User.findById(id,function(user,error){
+           done(err,user);
+       });
+   });
 }
